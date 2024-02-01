4858bc59859b23d2d0a510ff3d3074f8243fad5b
hfifre <hugo.fifre@gmail.com>
2024-02-01 12:27:00 +0100

    [1] moving spriteSheet (only left)

---
+++ spritesheets-demo/main.cpp
@@ -0,0 +1,100 @@
+#include "SFML/Graphics.hpp"
+
+void spriteAnimation(
+    int const& spriteSheetWidthSize,
+    int const& spriteSheetHeightSize,
+    int const& unitSize,
+    sf::IntRect &rectSourceSprite,
+    sf::Sprite &sprite)
+{
+    if (rectSourceSprite.left == spriteSheetWidthSize-unitSize)
+    {
+        rectSourceSprite.left = 0;
+        if (rectSourceSprite.top == spriteSheetHeightSize-unitSize)
+            rectSourceSprite.top = 0;
+        else
+            rectSourceSprite.top += unitSize;
+    }
+    else
+        rectSourceSprite.left += unitSize;
+
+    sprite.setTextureRect(rectSourceSprite);
+}
+
+int main(int argc, char** argv) {
+    sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "Demo Game");
+
+    sf::Event event;
+    sf::Texture texture;
+    texture.loadFromFile("ressources/perso_standing_still.png");
+
+    sf::IntRect rectSourceSprite(0, 0, 320, 320);
+    sf::Sprite sprite(texture, rectSourceSprite);
+    sf::Clock clock;
+    bool alreadyLeft = false;
+    bool alreadyRight = false;
+    bool alreadyNotMoving = false;
+
+    while (renderWindow.isOpen()) {
+        while (renderWindow.pollEvent(event)) {
+            if (event.type == sf::Event::EventType::Closed)
+                renderWindow.close();
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            if (!alreadyRight)
+            {
+                rectSourceSprite.left = 0;
+                rectSourceSprite.top = 0;
+                texture.loadFromFile("ressources/main_character_right.png");
+                alreadyRight = true;
+                alreadyNotMoving = false;
+                alreadyLeft = false;
+            }
+            if (clock.getElapsedTime().asMilliseconds() > 70.0f)
+            {
+                spriteAnimation(960, 1280, 320, rectSourceSprite, sprite);
+                clock.restart();
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            if (!alreadyLeft)
+            {
+                rectSourceSprite.left = 0;
+                rectSourceSprite.top = 0;
+                texture.loadFromFile("ressources/main_character_left.png");
+                alreadyLeft = true;
+                alreadyRight = false;
+                alreadyNotMoving = false;
+            }
+            if (clock.getElapsedTime().asMilliseconds() > 70.0f)
+            {
+                spriteAnimation(960, 1280, 320, rectSourceSprite, sprite);
+                clock.restart();
+            }
+        }
+        else
+        {
+            if (!alreadyNotMoving)
+            {
+                rectSourceSprite.left = 0;
+                rectSourceSprite.top = 0;
+                texture.loadFromFile("ressources/perso_standing_still.png");
+                alreadyNotMoving = true;
+                alreadyRight = false;
+                alreadyLeft = false;
+            }
+            if (clock.getElapsedTime().asMilliseconds() > 200.0f)
+            {
+                spriteAnimation(640, 960, 320, rectSourceSprite, sprite);
+                clock.restart();
+            }
+        }
+
+        renderWindow.clear();
+        renderWindow.draw(sprite);
+        renderWindow.display();
+    }
+}
